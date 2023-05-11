8ee7bd4d8eae2d089587dd45d28ddffc542910fa
wbs <1134954328@qq.com>
2023-05-11 22:52:11 +0800

    初步实现 AuthRoute 路由拦截

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
 import { Router } from "./routers/router"
+import { AuthRouter } from "./routers/AuthRouter"
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Router />
+      <AuthRouter>
+        <Router />
+      </AuthRouter>
     </BrowserRouter>
   )
 }
