09fc5eaf306655820aabc0daf5573716607ca71e
wbs <1134954328@qq.com>
2023-05-12 17:25:06 +0800

    引入 unocss

--- src/routers/LazyLoad.tsx
+++ src/routers/LazyLoad.tsx
@@ -3,13 +3,9 @@ import React, { Suspense } from "react"
 
 export const LazyLoad = (Comp: React.LazyExoticComponent<(props: any) => JSX.Element>): React.ReactNode => {
   return <Suspense fallback={
-    <Spin size='large' style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100%'
-    }} />
-  }
+    <Spin size='large'
+      className="flex justify-center items-center h-100%"
+    />}
   >
     <Comp />
   </Suspense>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unocss from 'unocss/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    Unocss(),
+    react()
+  ],
   server: {
     proxy: {
       '/api': {
